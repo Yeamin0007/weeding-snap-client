8c9cfe5d9c7e5c2d58ae11946f8a3a1f713ac108
Yeamin0007 <yh.hossainsaaz@gmail.com>
2022-11-08 01:57:26 +0600

    commit login page

--- src/Components/pages/Login/Login.js
+++ src/Components/pages/Login/Login.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className="hero w-full my-20">
+            <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <img className='w-3/4' src='https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-135.jpg?w=2000' alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full  shadow-2xl max-w-md bg-emerald-200 py-20">
+                    <h1 className="text-5xl text-center font-bold text- divide-cyan-900">Login</h1>
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-accent" type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <p className='text-center'>New to Wedding Snap? <Link className='text-divide-cyan-900 font-bold' to="/register">Sign Up</Link> </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/pages/Register/Register.js
+++ src/Components/pages/Register/Register.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            
+        <div className="hero w-full my-20">
+        <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+            <div className="text-center lg:text-left">
+                <img className='w-3/4' src='https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-135.jpg?w=2000' alt="" />
+            </div>
+            <div className="card flex-shrink-0 w-full  shadow-2xl max-w-md bg-emerald-200 py-20">
+                <h1 className="text-5xl text-center font-bold text- divide-cyan-900">Sign Up</h1>
+                <form className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <input className="btn btn-accent" type="submit" value="Sign Up" />
+                    </div>
+                </form>
+                <p className='text-center'>Already Have an Accout? <Link className='text- divide-cyan-900 font-bold' to="/login">Login</Link> </p>
+            </div>
         </div>
+    </div>
     );
 };
 
